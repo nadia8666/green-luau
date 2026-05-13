b970452f622dbf45c57e02c2b30e686c54ce887e
nadia <115858781+nadia8666@users.noreply.github.com>
2026-05-12 19:47:20 -0700

    add hopefully functional build script

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ it's good for the environment
 
 ts/java like language that is transpiled into luau with a blazingly slow transpiler. <br>
 code is "documented"
+
+## why?
+airship is switching to godot using luau and i dont like luau. this is as close to ts built from the ground up for airship's luau as you can get.
+
+### building
+probably don't. the go script is meant for a github action that isnt super well tested, if you the one reading this feel like being the one to write good go code. go,
---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"embed"
+	"io/fs"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+//go:embed lute.exe src/main.luau src/components/*.luau
+var embedded embed.FS
+
+func main() {
+	dir, err := os.MkdirTemp("", "green_luau")
+
+	// does go have inline if statements like if (x) y?
+	if err != nil {
+		os.Exit(1)
+	}
+
+	// I LOVE WALKING DIRECTORIES
+	err = fs.WalkDir(embedded, ".", func(path string, d fs.DirEntry, err error) error {
+		if err != nil || path == "." {
+			return err
+		}
+
+		targetPath := filepath.Join(dir, path)
+
+		if d.IsDir() {
+			return os.MkdirAll(targetPath, 0755)
+		}
+
+		content, err := embedded.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		return os.WriteFile(targetPath, content, 0755)
+	})
+
+	if err != nil {
+		os.Exit(1)
+	}
+
+	lute := filepath.Join(dir, "lute.exe")
+	entry := filepath.Join(dir, "src", "main.luau")
+
+	// this syntax is evil for what it acutally does i spent 10 minutes on this
+	cmd := exec.Command(lute, append([]string{entry}, os.Args[1:]...)...)
+	cmd.Dir = filepath.Join(dir, "src")
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	cmd.Run()
+}
